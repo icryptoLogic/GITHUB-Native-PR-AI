1625703319d9777c4a7de57e2c1e23f5b17085c8
IcryptoLogic <98883191+icryptoLogic@users.noreply.github.com>
2025-09-22 13:38:27 +0530

    Update test_app.py

--- src/test_app.py
+++ src/test_app.py
@@ -1,1 +1,10 @@
+import pytest
+from app import add
+
+def test_addition_success():
+    assert add(2, 3) == 5
+
+def test_addition_fail():
+    # ❌ Intentional failure for demo
+    assert add(2, 2) == 5
 
