6db49256385e4ae9c91d08b421120094d604110c
IcryptoLogic <98883191+icryptoLogic@users.noreply.github.com>
2025-10-30 01:02:58 +0530

    Update test_app.py
    MultiplePR

--- src/test_app.py
+++ src/test_app.py
@@ -2,9 +2,10 @@ import pytest
 from app import add
 
 def test_addition_success():
-    assert add(2, 3) == 4
+    assert add(2, 3) == 5
 
 def test_addition_fail():
     # ❌ Intentional failure for demo
-    assert add(2, 2) == 5
+    assert add(2, 2) == 4
+    #multiplesummaryPRTESTNoTESTFailed
 
